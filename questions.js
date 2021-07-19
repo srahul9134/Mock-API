5b1ef50b1b0382e8372c26e02734e70aca431938
SHINDE RAHUL DNYANESHWAR <43510570+srahul9134@users.noreply.github.com>
2021-07-20 00:47:16 +0530

    Add

---
+++ questions.js
@@ -0,0 +1,52 @@
+
+{
+  "questions": [
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+   {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+  {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+    {
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },{
+     "question_number": 1,
+     "question": "Overall, how satisfied are you with life as a whole these days?"
+    },
+  ],
+}
