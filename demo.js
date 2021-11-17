f86710927a18bccbd3ad9b24f75e2f9770079f41
SHINDE RAHUL DNYANESHWAR <43510570+srahul9134@users.noreply.github.com>
2021-11-18 00:38:59 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,51 +1,7 @@
 {
-  "questions": [
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-   {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-  {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },
-    {
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    },{
-     "question_number": 1,
-     "question": "Overall, how satisfied are you with life as a whole these days?"
-    }
+  "data": [
+  {"audio": "https://traffic.libsyn.com/secure/childlifeoncall/Episode_88___Life_On_Pause_-_9_28_21_3.44_PM.mp3", "description": "Life On Pause", "id": 28, "title": "Life On Pause"},
+   {"audio": "https://traffic.libsyn.com/secure/childlifeoncall/FINAL-CLC_EP087_FN_adok.mp3", "description": "Lisa and Brady's Story", "id": 29, "title": "Lisa and Brady's Story"},
+   {"audio": "https://cloc-app.s3.us-east-2.amazonaws.com/soothing_sounds/audioblocks-storytime-music-box-extended-version_SZ7O-Lt8v-SBA-300515406.mp3", "description": "https://cloc-app.s3.us-east-2.amazonaws.com/soothing_sounds/audioblocks-storytime-music-box-extended-version_SZ7O-Lt8v-SBA-300515406.mp3", "id": 39, "title": "Podcast"}
   ]
 }
